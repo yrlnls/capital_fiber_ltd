f189f5b204a222be61d426056954161053b42c2a
Abdi Mohamed <bashanesix@gmail.com>
2025-02-16 11:16:24 +0300

    added pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import 'font-awesome/css/font-awesome.min.css';
 
 function Home() {
   return (
-    <section id="home" className="container">
+    <div id="home">
       <img src="/CapitalLogo1.jpeg" alt="Capital Fiber Solutions Logo" className="logo img-fluid" />
       <h2 className="display-4">
         <i className="fa fa-home" style={{ fontSize: '2rem' }}></i> 
         Welcome to Capital Fiber Solutions
       </h2>
       <p className="lead">Your reliable provider of high-speed fiber internet and security solutions.</p>
-    </section>
+    </div>
   );
 }
 
