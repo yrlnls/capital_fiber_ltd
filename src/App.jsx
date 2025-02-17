a4132d1824d515a5c3cf976d8987c0b457d31f0b
Abdi Mohamed <bashanesix@gmail.com>
2025-02-17 11:03:21 +0300

    drop-down menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,34 +7,31 @@ import Services from './components/Services';
 import Packages from './components/Packages';
 import Security from './components/Security';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
     <Router>
-      <header className="header">
-        <img src="/CapitalLogo1.jpeg" alt="Capital Fiber Solutions Logo" className="logo" />
-        <p className="customer-service">CUSTOMER SERVICE 24/7: 0714001616/0112274074</p>
-        <nav className="nav">
-          <ul className="nav-list">
-            <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
-            <li className="nav-item"><a href="/packages" className="nav-link">Packages</a></li>
-            <li className="nav-item"><a href="/services" className="nav-link">Services</a></li>
-            <li className="nav-item"><a href="/contact" className="nav-link">Contact Us</a></li>
-            <li className="nav-item"><a href="/about" className="nav-link">About Us</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/packages" element={<Packages />} />
-          <Route path="/security" element={<Security />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Footer />} />
-        </Routes>
-      </main>
-      <Footer />
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/packages" element={<Packages />} />
+            <Route path="/security" element={<Security />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Footer />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
